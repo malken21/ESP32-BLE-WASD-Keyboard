58e5822b5321154b8db7d49b4f41fa352aff092a
Marumasa <77374813+malken21@users.noreply.github.com>
2024-10-09 16:44:21 +0900

    Create config.h

---
+++ keyboard/config.h
@@ -0,0 +1,11 @@
+// WASD のキーのピン番号
+#define PIN_W 16
+#define PIN_A 17
+#define PIN_S 18
+#define PIN_D 19
+
+// キーを押したときに点灯するLEDのピン番号
+#define PIN_LED 2
+
+// デバイスの名前を指定できます
+#define KEYBOARD_NAME "WASD Key 1.0"
